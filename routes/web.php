ffe0a523497f9439f3e3f4ec11028b15d21393a3
adzanilrachmadhip <adzanilrachmadip@gmail.com>
2026-04-28 14:34:53 +0700

    fix dashboard

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,6 @@ use App\Http\Controllers\EmployeeController;
 use Illuminate\Support\Facades\Route;
 
 // ==================== LOGIN ====================
-Route::get('/', function(){
-    view('dashboard')
-};
-
 
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login'])->name('login.post');
